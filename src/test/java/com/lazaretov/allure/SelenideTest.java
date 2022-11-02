b34433b504a189825be277a68a8b93731e90c012
Timofei <lazaretov03@gmail.com>
2022-11-02 15:47:13 +0600

    add Listener allure report

--- src/test/java/com/lazaretov/allure/SelenideTest.java
+++ src/test/java/com/lazaretov/allure/SelenideTest.java
@@ -1,6 +1,9 @@
 package com.lazaretov.allure;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
@@ -12,7 +15,10 @@ import static org.openqa.selenium.By.linkText;
 public class SelenideTest {
 
     @Test
+    @DisplayName("Поиск Issue по номеру")
     public void issueSearchTest () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         open("https://github.com/");
 
         $(".header-search-input").click();
