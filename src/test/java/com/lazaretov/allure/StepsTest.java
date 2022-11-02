85f0e7490775fb2af95ba9ae180efb7a2594381f
Timofei <lazaretov03@gmail.com>
2022-11-02 15:48:57 +0600

    add StepsTest.java + lambda function

---
+++ src/test/java/com/lazaretov/allure/StepsTest.java
@@ -0,0 +1,64 @@
+package com.lazaretov.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+
+    public static final String REPOSITORY = "eroshenkoam/allure-example";
+    public static final int ISSUE = 80;
+
+
+    @Test
+    @DisplayName("Поиск Issue по номеру")
+    public void lambdaStepTest () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com/");
+        });
+
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").setValue(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+
+        step("Переходим в репозиторий " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("Открываем вкладку Issues", () -> {
+            $("#issues-tab").click();
+        });
+
+        step("Проверяем наличие записи с номер #" + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
